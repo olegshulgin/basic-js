ce28586437d470cd282e224ee08f57b5821b4d5b
olegshulgin <97721233+olegshulgin@users.noreply.github.com>
2022-10-14 15:09:28 +0300

    feat: update

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
+function dateSample(sampleActivity) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  if (
+    typeof sampleActivity === 'string' &&
+    Number(sampleActivity) <= MODERN_ACTIVITY &&
+    Number(sampleActivity) > 0
+  ) {
+    return Math.ceil(
+      Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD),
+    );
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
