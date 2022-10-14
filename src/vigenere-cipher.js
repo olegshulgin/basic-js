aa6086f783239440a89eb3e602241bf402c2e84d
olegshulgin <97721233+olegshulgin@users.noreply.github.com>
2022-10-14 22:31:24 +0300

    feat: update

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,94 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(state = true) {
+    this.state = state;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  alphabetArray = this.alphabet.split('');
+  
+  encrypt(message, key) {
+    // throw new NotImplementedError('Not implemented');
+    if(!message || !key) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    console.log(key)
+
+    let idMassage = [];
+    let idKey = [];
+    let specialSymbols = [];
+    let idRes;
+    let res;
+    message = message.toLowerCase();
+    key = key.toLowerCase();
+
+    const messageArray = message.split('');
+    messageArray.forEach((element, index) => {
+      if(this.alphabetArray.indexOf(element) === -1) {
+        specialSymbols.push([element,index])
+      }
+
+      return idMassage.push(this.alphabetArray.indexOf(element))
+    });
+    idMassage = idMassage.filter(element => element !== -1)
+
+    let newKey = key.repeat(idMassage.length).slice(0, idMassage.length);
+    const keyArray = newKey.split('');
+    keyArray.forEach(element => idKey.push(this.alphabetArray.indexOf(element))); 
+
+    idRes = idMassage.map( (element, index) => element === - 1 ? ' ' : element + idKey[index]);
+    res = idRes.map(element => element === ' ' ? ' ' : this.alphabetArray[element] ? this.alphabetArray[element] : this.alphabetArray[element - this.alphabetArray.length]);
+    specialSymbols.forEach(element => {
+      res.splice(element[1], 0, element[0])
+    })
+    res = res.join('').toUpperCase();
+
+    return res;
+  }
+  decrypt(message, key) { 
+    // throw new NotImplementedError('Not implemented');
+    if(!message || !key) {
+      throw new Error('Incorrect arguments!')
+    }
+
+    let idMassage = [];
+    let idKey = [];
+    let specialSymbols = [];
+    let idRes;
+    let res;
+    message = message.toLowerCase();
+    key = key.toLowerCase();
+
+    const messageArray = message.split('');
+    messageArray.forEach((element, index) => {
+      if(this.alphabetArray.indexOf(element) === -1) {
+        specialSymbols.push([element,index])
+      }
+
+      return idMassage.push(this.alphabetArray.indexOf(element))
+    });
+    idMassage = idMassage.filter(element => element !== -1)
+
+    let newKey = key.repeat(idMassage.length).slice(0, idMassage.length);
+    const keyArray = newKey.split('');
+    keyArray.forEach(element => idKey.push(this.alphabetArray.indexOf(element))); 
+    const reverseIdKey = idKey.map(el => String(el)).reverse().map(el => +el)
+
+    if(this.state) {
+      idRes = idMassage.map( (element, index) => element === - 1 ? ' ' : element > idKey[index] ? element - idKey[index] : element - idKey[index] + this.alphabetArray.length)
+    } else {
+      idRes = idMassage.map( (element, index) => element === - 1 ? ' ' : element > reverseIdKey[index] ? element - reverseIdKey[index] : element - reverseIdKey[index] + this.alphabetArray.length)
+    }
+
+    res = idRes.map(element => element === ' ' ? ' ' : this.alphabetArray[element] ? this.alphabetArray[element] : this.alphabetArray[element - this.alphabetArray.length]);
+    specialSymbols.forEach(element => {
+      res.splice(element[1], 0, element[0])
+    })
+    res = res.join('').toUpperCase();
+
+    return res;
   }
 }
 
