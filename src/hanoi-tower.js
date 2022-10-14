a5b9e23cffc51b8aca7973d8213a39295128b068
olegshulgin <97721233+olegshulgin@users.noreply.github.com>
2022-10-14 15:02:59 +0300

    feat: update1

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -15,7 +15,6 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function calculateHanoi(disksNumber, turnsSpeed) {
-  throw new NotImplementedError('Not implemented');
   let turns = Math.pow(2, disksNumber) - 1;
   let seconds = Math.floor((turns * 3600) / turnsSpeed);
   return { turns, seconds };
